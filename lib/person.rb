e4252ea9651e22e6bc960df85561eec587540bb4
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-06-13 10:47:14 +0300

    commit tree 438aaa9e3891016756d0759582d2f1663be824a8

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
 
+    def name=(name)
+      @name = name
+    end
+
+    def name
+      @name
+    end
+
+    def job=(job)
+      @job = job
+    end
+
+    def job
+      @job
+    end
+
 end
+
+ann = Person.new
+ann.name = "Ann"
+
+economist = Person.new
+economist.job = "Economist"
